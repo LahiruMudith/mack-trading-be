5da671eb6020dbad0a18da624101540b42587176
Lahiru_Mudith <lahimudith@gmail.com>
2025-12-03 09:54:45 +0530

    create token & refresh token

---
+++ src/utils/refreshToken.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import {IUser} from "../model/User";
+import jwt from "jsonwebtoken";
+
+dotenv.config()
+
+const JWT_REFRESH_SECRETS = process.env.JWT_REFRESH_SECRETS as string;
+
+export const signRefreshToken = (user : IUser) => {
+    return jwt.sign(
+        {
+            _id:user._id
+        },
+        JWT_REFRESH_SECRETS,
+        {
+            expiresIn:'7d'
+        }
+    )
+}
---
+++ src/utils/token.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import {IUser} from "../model/User";
+import jwt from "jsonwebtoken"
+
+dotenv.config()
+
+const JWT_SECRETS = process.env.JWT_SECRETS as string;
+
+export const signAccessToken = (user:IUser)=>{
+    return jwt.sign(
+        {
+            _id:user._id,
+            email:user.email,
+            role:user.role
+        },
+        JWT_SECRETS,
+        {
+            expiresIn:'1d'
+        }
+    )
+}
