28c39814fe95af1f8dcbdd4a3e012820b085097d
Lahiru_Mudith <lahimudith@gmail.com>
2026-01-16 13:19:15 +0530

    add cros url

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const express = require('express')
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://mack-trading-be.vercel.app"],
     credentials: true
 }))
 
