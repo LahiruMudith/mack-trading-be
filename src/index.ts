73c180ebf80a9b7ff0176d8bbdda860af2d273c8
Lahiru_Mudith <lahimudith@gmail.com>
2026-01-16 19:34:18 +0530

    delete test url in cros

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,7 @@ const express = require('express')
 const app = express()
 
 app.use(cors({
-    origin: [
-        // "http://localhost:5173",
-        "https://mack-trading-fn-4td6.vercel.app"  // <--- ADD THIS EXACT URL
-    ],
+    origin:"https://mack-trading-fn-4td6.vercel.app" , // <--- ADD THIS EXACT URL,
     credentials: true
 }))
 
