5fe7fd30d13f1b119a78b7ee35aed1e2b8e96b35
Lahiru_Mudith <lahimudith@gmail.com>
2026-01-16 19:44:33 +0530

    syntax error

--- src/util/db.ts
+++ src/util/db.ts
@@ -10,7 +10,7 @@ const connectDB = async () => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGO_URI as string);
+        await mongoose.connect(process.env.MONGO_URL as string);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error);
