4737bf4b7c908fd0d9568c47c8d9af4745c37e1c
Lahiru Mudith <148560361+LahiruMudith@users.noreply.github.com>
2026-02-27 11:19:43 +0530

    configure CORS options and handle preflight requests

--- src/index.ts
+++ src/index.ts
@@ -18,17 +18,10 @@ const SERVER_PORT = process.env.SERVER_PORT
 const express = require('express')
 const app = express()
 
-const corsOptions = {
+app.use(cors({
     origin: ["http://localhost:5173", "https://mack-trading-fn-4td6.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
-};
-
-app.use(cors(corsOptions));
-
-// Explicitly handle Preflight requests
-app.options('*', cors(corsOptions));
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
